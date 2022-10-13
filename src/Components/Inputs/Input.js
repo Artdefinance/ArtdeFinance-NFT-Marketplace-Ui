3e463884f49dc02ca56dc33c90b7db2858a0a67d
khc <khc@ona.co.kr>
2022-10-13 20:32:40 +0900

    input 삭제버튼 동작 수정

--- src/Components/Inputs/Input.js
+++ src/Components/Inputs/Input.js
@@ -1,94 +1,54 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-import './Input.scss';
+import { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import Icons from '../Icons/Icons';
+import './Input.scss';
 
-export default class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      isClearButton: false,
-    };
-    this.showCloseButton = this.showCloseButton.bind(this);
-    this.clearText = this.clearText.bind(this);
-  }
+function Input({ name, disabled, placeholder, getValue }) {
+  const [value, setValue] = useState(getValue || '');
+  const inputRef = useRef(null);
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  const clearText = () => {
+    setValue('');
+    inputRef?.current?.focus?.();
+  };
 
-  showCloseButton(e) {
-    if (e.target.value === '') {
-      this.setState(() => ({
-        [e.target.name]: false,
-        isClearButton: false,
-        value: '',
-      }));
-    }
-    if (e.target.value !== '') {
-      this.setState(() => ({
-        value: e.target.value,
-        isClearButton: true,
-      }));
-    }
-    if (e.target.value && [e.target.name]) {
-      this.setState(() => ({
-        [e.target.name]: true,
-        isClearButton: true,
-      }));
-    }
-  }
-
-  clearText() {
-    this.setState(() => ({
-      value: '',
-      isClearButton: false,
-    }));
-  }
-
-  render() {
-    const { value, isClearButton } = this.state;
-    const { name, disabled, placeholder, getValue } = this.props;
-    return (
-      <div className={disabled === 'true' ? 'input disabled' : 'input'}>
-        <div className="input__input-box">
-          {disabled === 'true' ? (
-            <input
-              type="text"
-              className="input__input"
-              placeholder={placeholder}
-              name={name}
-              onChange={this.showCloseButton}
-              value={value}
-              disabled
-            />
-          ) : (
-            <div>
-              {getValue === '' ? (
-                <input
-                  type="text"
-                  className="input__input"
-                  placeholder={placeholder}
-                  name={name}
-                  onChange={this.showCloseButton}
-                  value={value}
-                />
-              ) : (
-                <input
-                  type="text"
-                  className="input__input"
-                  placeholder={placeholder}
-                  name={name}
-                  onChange={this.showCloseButton}
-                  value={getValue}
-                />
-              )}
-            </div>
-          )}
-          {isClearButton ? (
-            <button type="button" onClick={this.clearText} name={name}>
-              <Icons shape="close-circle" />
-            </button>
-          ) : null}
-        </div>
+  return (
+    <div className={clsx('input', disabled === 'true' && 'disabled')}>
+      <div className="input__input-box">
+        <input
+          ref={inputRef}
+          type="text"
+          className="input__input"
+          placeholder={placeholder}
+          name={name}
+          onChange={onChange}
+          value={value}
+          disabled={disabled === 'true'}
+        />
+        {!!value && (
+          <button type="button" onClick={clearText} name={name}>
+            <Icons shape="close-circle" />
+          </button>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Input.defaultProps = {
+  disabled: '',
+  placeholder: '',
+  getValue: '',
+};
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  disabled: PropTypes.string,
+  placeholder: PropTypes.string,
+  getValue: PropTypes.string,
+};
+
+export default Input;
--- src/Components/ItemCardListFilter/FilterSideKeywordInput.js
+++ src/Components/ItemCardListFilter/FilterSideKeywordInput.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef, useId } from 'react';
 import PropTypes from 'prop-types';
 import Icons from '../Icons/Icons';
 
 function FilterSideKeywordInput({ placeholder, defaultValue }) {
   const [value, setValue] = useState(defaultValue);
+  const id = useId();
+  const inputRef = useRef(null);
+  const onClickDelete = () => {
+    setValue('');
+    inputRef?.current?.focus?.();
+  };
+
   return (
     <div className="itemcard-list-filter__keyword">
-      <label htmlFor="filter_keyword" className="itemcard-list-filter__keyword-label">
-        <input id="filter_keyword" type="text" onChange={(event) => setValue(event.target.value)} placeholder={placeholder} className="itemcard-list-filter__keyword-input" value={value} />
+      <label htmlFor={id} className="itemcard-list-filter__keyword-label">
+        <input id={id} ref={inputRef} type="text" onChange={(event) => setValue(event.target.value)} placeholder={placeholder} className="itemcard-list-filter__keyword-input" value={value} />
         <Icons shape="search_small" />
       </label>
-      <button type="button" className="itemcard-list-filter__keyword-delete" aria-hidden={!value}>
+      <button type="button" className="itemcard-list-filter__keyword-delete" aria-hidden={!value} onClick={onClickDelete}>
         <Icons shape="close-circle" />
       </button>
     </div>
