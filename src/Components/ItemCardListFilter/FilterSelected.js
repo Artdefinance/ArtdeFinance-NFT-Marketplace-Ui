1e87c1f92b47119b5be355c7667bc8cd8f6283fc
khc <khc@ona.co.kr>
2022-11-04 01:01:55 +0900

    필터 삭제 추가

--- src/Components/ItemCardListFilter/FilterSelected.js
+++ src/Components/ItemCardListFilter/FilterSelected.js
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import IconStyle from '../Icons/Icons';
 
 function FilterSelected({ selectedFilters }) {
-  if (!selectedFilters.length) return null;
+  const [filters, setFilters] = useState(selectedFilters);
+
+  const removeFilter = (name) => () => setFilters((f) => f.filter((item) => item !== name));
+  const clearAll = () => setFilters([]);
+
+  if (!filters.length) return null;
+
   return (
     <div className="itemcard-list-filter__selected">
       <ul className="itemcard-list-filter__selected-items">
-        {selectedFilters.map((name, index) => (
+        {filters.map((name, index) => (
           <div key={`${index.toString()}-${name}`} className="itemcard-list-filter__selected-item">
             {name}
-            <button type="button" className="itemcard-list-filter__btn-remove-selected">
+            <button type="button" className="itemcard-list-filter__btn-remove-selected" onClick={removeFilter(name)}>
               <span className="a11y">Remove</span>
               <IconStyle shape="close_small" />
             </button>
           </div>
         ))}
       </ul>
-      <button type="button" className="itemcard-list-filter__btn-clear-selected">
+      <button type="button" className="itemcard-list-filter__btn-clear-selected" onClick={clearAll}>
         Clear all
       </button>
     </div>
